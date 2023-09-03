5a489024a3c263b654c68d61f2adadcdc05ef72c
cyt <1793402316@qq.com>
2023-09-03 18:13:30 +0800

    build

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: '@vuepress/blog',
+    themeConfig: {
+        logo: '/assets/img/avatar.png',
+        // 导航栏链接
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/about' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    }
+}
